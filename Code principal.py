200b17ab12cdb422ea49727aa20a856f59b1fba1
Jeidkhn <jkhannouf@icloud.com>
2025-06-29 04:00:57 +0200

    Modification déplacement joueur

--- Code principal.py
+++ Code principal.py
@@ -1,11 +1,11 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((960, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 1.1)
 
 while running:
 
@@ -18,14 +18,11 @@ while running:
     pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= 600 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += 600 * dt
 
     pygame.display.flip()
 
